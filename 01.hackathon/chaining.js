2501dbcd8957cfc16618384920d8c86e125d0be9
Sharu24 <sharu.mukti@gmail.com>
2020-08-18 21:15:41 +0530

    added description

--- 01.hackathon/chaining.js
+++ 01.hackathon/chaining.js
@@ -23,3 +23,17 @@ helloWorld
   .a()
   .b()
   .c();
+
+/*
+Chaining in JavaScript is a Programming methodology
+where a sequence of methods/functions attached to
+and Object is called repeatedly.
+
+This is possible by returning 'this' keyword.
+'this' points to the Object wrapping the methods.
+Each method performs a task and returns back a
+reference to the Object in which its held. 
+The next function is invoked using the returned
+Object and so on... 
+
+  */
--- 01.hackathon/currying.js
+++ 01.hackathon/currying.js
@@ -14,6 +14,31 @@ console.log(result);
 
 /*
 
+Currying is programming style where 
+A function with multiple arguments is
+evaluated by nesting functions until
+all the arguments are exhausted.
+
+Steps
+1. Function returns a new function 
+2. The new function consumes an Argument
+3. Repeats the Process till the last argument
+
+Currying utilizes 
+1. JS closure paradigm where each function 
+    has access to its outer environment variable.
+2. A function returning another function
+
+Usage 
+1. log(date, method, message);
+    ._currying -> log(date)(method)(message)
+    now we can execut log(date) separately
+    today = log(Date.now());
+    today('DEBUG','message1')
+    today('DEBUG','message2')
+2. As we can see above, we can fetch the date 
+    and have a boilerplate ready for invoking
+    next set of Debug statements for today.
 
 
 */
