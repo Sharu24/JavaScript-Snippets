5c8103e64624156702c8a40040b12e050588b6e4
Sharu24 <sharu.mukti@gmail.com>
2020-08-19 07:33:56 +0530

    updated documentation

--- 01.hackathon/chaining.js
+++ 01.hackathon/chaining.js
@@ -1,8 +1,23 @@
 // jshint esversion:6
 // Chaining in Javascript
 
+/*
+Chaining in JavaScript is a Programming methodology
+where a sequence of methods/functions is invoked, 
+one after the other, by attaching them together 
+in the same line.
+
+Each method performs a task and returns back a
+reference to the Object in which its held. 
+the First Function is invoked and returns
+the Object back; The next function in the chain is invoked 
+using the returned Object and so on... 
+*/
+
 // Expected Output using Chaning methods in a Object
 // Hello World
+
+// Approach 1
 const helloWorld = {
   str: "",
   add(subs) {
@@ -17,29 +32,20 @@ const helloWorld = {
     return this;
   },
   c() {
-    this.add("World");
+    this.add("World 1");
     console.log(this.str);
   }
 };
 
+
+// Approach #1
 helloWorld
   .a()
   .b()
   .c();
-
 /*
-Chaining in JavaScript is a Programming methodology
-where a sequence of methods/functions part of an Object is 
-invoked, one after the other, by attaching them together 
-in the same line.
-
-Each method performs a task and returns back a
-reference to the Object in which its held. 
-Using the Object, the First Function is invoked and returns
-the Object back; The next function in the chain is invoked 
-using the returned Object and so on... 
 Returning the Object is achieved by returning 
-'this'  in every function call. 
+'this' in every function call. 
 'this' points to the Object containing the methods.
 
 chain1 = HelloWorld.a()
@@ -57,3 +63,25 @@ chain2.c()
   Calls the function add() using Object returned from HelloWorld.b()
   returns 'this' pointing to the current object
 */
+
+// Approach 2
+// In this approach, Every Function returns back an Object 
+// reference containing the reference of another function
+
+function x() {
+  return {
+    y: function() {
+      return {
+        z: function() {
+          console.log("Hello World 2");
+        }
+      };
+    }
+  };
+}
+
+// Approach #2
+x()
+  .y()
+  .z();
+
