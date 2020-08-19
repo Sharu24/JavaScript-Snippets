80adbf86424f9cc0c7b6a5b3e54c56996505bfbe
Sharu24 <sharu.mukti@gmail.com>
2020-08-19 06:20:22 +0530

    updated documentation

--- 01.hackathon/chaining.js
+++ 01.hackathon/chaining.js
@@ -29,14 +29,30 @@ helloWorld
 
 /*
 Chaining in JavaScript is a Programming methodology
-where a sequence of methods/functions attached to
-and Object is called repeatedly.
+where a sequence of methods/functions part of an Object is 
+invoked, one after the other, by attaching them together 
+in the same line.
 
-This is possible by returning 'this' keyword.
-'this' points to the Object wrapping the methods.
 Each method performs a task and returns back a
 reference to the Object in which its held. 
-The next function is invoked using the returned
-Object and so on... 
+Using the Object the First Function is invoked and returns
+the Object back; The next function in the chain is invoked 
+using the returned Object and so on... 
+Its achived by returning 'this' keyword  in every function call. 
+'this' points to the Object containing the methods.
 
+chain1 = HelloWorld.a()
+  Calls the function add() using Object HelloWorld
+  returns 'this' pointing to the current object
+
+  Note - returning this is important for 
+  subsequent functions to get invoked
+
+chain2 = chain1.b()
+  Calls the function add() using Object returned from HelloWorld.a()
+  returns 'this' pointing to the current object
+
+chain2.c()
+  Calls the function add() using Object returned from HelloWorld.b()
+  returns 'this' pointing to the current object
 */
